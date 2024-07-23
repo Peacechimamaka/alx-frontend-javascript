dd96e0f90646cb2c94428b1eac48942880f245bb
Peacechimamaka <peaceamaka111@gmail.com>
2024-07-23 16:41:29 +0100

    1-block-scoped.js

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueOrFalse) {
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    const task = true; //eslint-disable-line no-unused-vars
+    const task2 = false; // eslint-diable-line no unused-vars
+  }
+
+  return [task, task2];
+}
