33e597dbfef185ed059dda1c554e690cf3dccfcc
Peacechimamaka <peaceamaka111@gmail.com>
2024-07-27 00:31:36 +0100

    tasks

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,28 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Cloning = this.constructor[Symbol.species];
+    return new Cloning(this._brand, this._motor, this._color);
+  }
+}
---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,28 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._brand;
+  }
+
+  get color() {
+    return this._brand;
+  }
+
+  get range() {
+    return this._brand;
+  }
+
+  cloneCar() {
+    return new this.constructor(this._brand, this.motor, this.color, this.range);
+  }
+}
